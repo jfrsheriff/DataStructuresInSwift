d9585a43439afc2e42a95a1205de946194711fd9
Jaffer Sheriff <jfrsheriff@gmail.com>
2022-05-23 22:54:40 +0530

    Graph Implementation
    Graph Implementation with Adjacency List

---
+++ Graph.playground/Sources/Vertex.swift
@@ -0,0 +1,19 @@
+
+public struct Vertex<Element>{
+    public var index : Int
+    public var value : Element
+    public init(withIndex index : Int, value : Element) {
+        self.index = index
+        self.value = value
+    }
+}
+
+extension Vertex : Hashable where Element : Hashable{}
+extension Vertex : Equatable where Element : Equatable{}
+
+extension Vertex : CustomStringConvertible{
+    public var description: String {
+        "\(index) : \(value)"
+    }
+}
+
